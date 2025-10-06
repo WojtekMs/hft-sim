f473250c286fd98b6728192331a199363c5c1412
Wojciech Matusiak <62173407+WojtekMs@users.noreply.github.com>
2025-10-06 14:34:20 +0200

    Format CMake files

--- cmake/FetchCatch2.cmake
+++ cmake/FetchCatch2.cmake
@@ -1,10 +1,6 @@
-Include(FetchContent)
+include(FetchContent)
 
-FetchContent_Declare(
-Catch2
-GIT_REPOSITORY https://github.com/catchorg/Catch2.git
-GIT_TAG        v3.8.1
-)
+FetchContent_Declare(Catch2 GIT_REPOSITORY https://github.com/catchorg/Catch2.git GIT_TAG v3.8.1)
 
 FetchContent_MakeAvailable(Catch2)
 
